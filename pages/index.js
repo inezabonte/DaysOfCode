a92d8a21fa0916e8306c0484e3c7e4717df07c19
inezabonte <inezabonte@gmail.com>
2021-04-21 20:14:56 +0200

    landing page complete

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+
+export default function index() {
+    return (
+        <div className='h-screen text-gray-100 flex flex-col justify-center text-center items-center'>
+            <h2 className='font-bold text-2xl m-10'>Welcome to the #100DaysOfCode challenge</h2>
+            <p className='text-xl m-10'>Sign in with your twitter account to get daily reminders to code.</p>
+            <button className='bg-blue-400 p-2 font-bold m-4'>Sign in with twitter</button>
+        </div>
+    )
+}
